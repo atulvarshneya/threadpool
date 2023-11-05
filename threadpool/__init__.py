6e5110d6ebdb007a631ddae3bee56f779771adeb
Atul Varshneya <atul.varshneya@gmail.com>
2023-11-05 12:33:33 -0800

    removed redundant code

--- threadpool/__init__.py
+++ threadpool/__init__.py
@@ -3,21 +3,3 @@
 from .executor import Executor
 from .as_completed import as_completed
 
-
-if __name__ == '__main__':
-	import time
-	
-	def func(a, b):
-		sleeptime = a*2+1
-		print(f'func({a}, {b}), sleeping for {sleeptime}...')
-		time.sleep(sleeptime)
-		print(f'func({a},{b}) DONE')
-
-	executor = Executor(nworkers=3)
-
-	futures = [executor.submit(func, i, b='param') for i in range(5)]
-
-	executor.wait(futures[2])
-	print('wait() for future 2 over')
-
-	executor.shutdown()
